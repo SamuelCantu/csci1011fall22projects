2b3a2170a5ab69bac322e34d9df35e8e84465c55
Samuel Cantu <sam.cantu2002@gmail.com>
2022-11-01 19:05:36 -0500

    Add web project

---
+++ web-and-mobile/html-css-js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function ()
+{
+    //Get click element references.
+    let clickCounterElement = document.getElementById("click-counter");
+    let clcikButtonElement = document.getElementById("clcik-button");
+
+    //Counter value.
+    let counter = 0;
+
+    //Cliclk button function.
+    let clickButtonFunction = function ()
+    {
+        //Increment counter
+        counter++;
+
+        //Update counter value
+        clickCounterElement.innerHTML = counter;
+    };
+
+    //Attach button function.
+    clcikButtonElement.addEventListener("click", clickButtonFunction);
+});
