fcdbd24e6919b169687352a19ce20e482b8a1ea0
Samuel Cantu <sam.cantu2002@gmail.com>
2022-11-22 17:28:55 -0600

    Add games data project

---
+++ games-data/games-released-birthyear-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE first_release_date BETWEEN '2002-11-25 00:00:00' AND '2002-12-31 23:59:59' ORDER BY name;
---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (7049108108, 1, 'Hello teacher!');
+
+--UPDATE sandbox SET double_value = 2.1543054 WHERE id = 132;
+
+--DELETE FROM sandbox WHERE id = 76
+
+SELECT * FROM sandbox;
